4145a726f5cbf54357aee73ed941248be9ef29ba
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-30 10:29:22 +0900

    1 file modified

--- src/app/shared/highlight.directive.spec.ts
+++ src/app/shared/highlight.directive.spec.ts
@@ -1,57 +1,76 @@
 import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-
 import { HighlightDirective } from './highlight.directive';
 
+//これらのテストはHighlightDirectiveが正しく動作すること、また期待される範囲で適用されることを確認しています。
+
+// テスト用のComponentを作成します。
 @Component({
   template: `
   <h2 highlight="yellow">Something Yellow</h2>
   <h2 highlight>The Default (Gray)</h2>
   <h2>No Highlight</h2>
-  <input #box [highlight]="box.value" value="cyan"/>`
+  <input #box [highlight]="box.value" value="cyan"/>`,
 })
-class TestComponent { }
+class TestComponent {}
 
+// HighlightDirectiveに関するテストをグループ化します。
 describe('HighlightDirective', () => {
-
+  // 変数の定義
+  //テストのために作成されるコンポーネントの固定値です。各テストケースで利用されます。
   let fixture: ComponentFixture<TestComponent>;
-  let des: DebugElement[];  // the three elements w/ the directive
-  let bareH2: DebugElement; // the <h2> w/o the directive
+  //ディレクティブが適用されている3つの要素を表すDebugElementの配列です。
+  let des: DebugElement[];
+  //ディレクティブが適用されていないh2要素を表すDebugElementです。
+  let bareH2: DebugElement;
 
+  // 各テストケース実行前の共通処理
+  //テストモジュールの設定やテスト対象のコンポーネントの初期化、
+  //初期のデータバインディング、そしてテスト対象の要素の取得が行われています。
   beforeEach(() => {
+    // テスト対象のコンポーネントを設定
     fixture = TestBed.configureTestingModule({
-      declarations: [ HighlightDirective, TestComponent ]
-    })
-    .createComponent(TestComponent);
+      //declarationsにテストするコンポーネントとディレクティブを指定
+      declarations: [HighlightDirective, TestComponent],
+    }).createComponent(TestComponent);
+    // TestComponentを作成
 
-    fixture.detectChanges(); // initial binding
+    // 初期のデータバインディング
+    fixture.detectChanges();
 
-    // all elements with an attached HighlightDirective
+    // ディレクティブが適用されている全ての要素を取得
     des = fixture.debugElement.queryAll(By.directive(HighlightDirective));
 
-    // the h2 without the HighlightDirective
+    // ディレクティブが適用されていないh2要素を取得
     bareH2 = fixture.debugElement.query(By.css('h2:not([highlight])'));
   });
 
-  // color tests
+  // テスト1: ハイライトされている要素が3つあることを確認
+  //toBeメソッドは、期待値が特定の値と厳密に等しいかどうかを検証するためのものです。
+  // des.lengthは、指定したクラスまたはセレクタを持つDOM要素の配列の長さを返します。ここでは3つの要素が期待されています。
   it('should have three highlighted elements', () => {
     expect(des.length).toBe(3);
   });
 
+  // テスト2: 1つ目の<h2>の背景色が"yellow"であることを確認
+  // des[0].nativeElement.style.backgroundColorは、最初のハイライトされた要素の背景色を取得します。
   it('should color 1st <h2> background "yellow"', () => {
     const bgColor = des[0].nativeElement.style.backgroundColor;
     expect(bgColor).toBe('yellow');
   });
 
+  // テスト3: 2つ目の<h2>の背景色がデフォルト色であることを確認
+  // HighlightDirectiveのdefaultColorプロパティの値が、2つ目のハイライトされた要素の背景色と一致していることを確認します。
   it('should color 2nd <h2> background w/ default color', () => {
     const dir = des[1].injector.get(HighlightDirective) as HighlightDirective;
     const bgColor = des[1].nativeElement.style.backgroundColor;
     expect(bgColor).toBe(dir.defaultColor);
   });
 
+  // テスト4: <input>の背景色がvalue色とバインドされていることを確認
+  // <input>要素の背景色が、そのvalueプロパティにバインドされていることを確認します。
   it('should bind <input> background to value color', () => {
-    // easier to work with nativeElement
     const input = des[2].nativeElement as HTMLInputElement;
     expect(input.style.backgroundColor)
       .withContext('initial backgroundColor')
@@ -59,7 +78,6 @@ describe('HighlightDirective', () => {
 
     input.value = 'green';
 
-    // Dispatch a DOM event so that Angular responds to the input value change.
     input.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
@@ -68,44 +86,39 @@ describe('HighlightDirective', () => {
       .toBe('green');
   });
 
+  // テスト5: ディレクティブが適用されていないh2要素はcustomPropertyを持っていないことを確認
+  // ディレクティブが適用されていない要素には、customPropertyが存在しないはずです。
+  it('bare <h2> should not have a customProperty', () =>
+    it('bare <h2> should not have a customProperty', () => {
+      expect(bareH2.properties['customProperty']).toBeUndefined();
+    }));
 
-  it('bare <h2> should not have a customProperty', () => {
-    expect(bareH2.properties['customProperty']).toBeUndefined();
-  });
-
-  // Removed on 12/02/2016 when ceased public discussion of the `Renderer`. Revive in future?
-  // // customProperty tests
-  // it('all highlighted elements should have a true customProperty', () => {
-  //   const allTrue = des.map(de => !!de.properties['customProperty']).every(v => v === true);
-  //   expect(allTrue).toBe(true);
-  // });
-
-  // injected directive
-  // attached HighlightDirective can be injected
+  // テスト6: 1つ目の<h2>にHighlightDirectiveが注入できることを確認
+  // des[0].injector.get(HighlightDirective)は、最初のハイライトされた要素にHighlightDirectiveが注入されているか確認します。
+  // toBeTruthy()は、返される値が真偽値の真(true)に評価されるかどうかをテストします。
   it('can inject `HighlightDirective` in 1st <h2>', () => {
     const dir = des[0].injector.get(HighlightDirective);
     expect(dir).toBeTruthy();
   });
 
+  // テスト7: 3つ目の<h2>にHighlightDirectiveが注入できないことを確認
+  // bareH2.injector.get(HighlightDirective, null)は、ハイライトされていない3つ目の要素にHighlightDirectiveが注入されていないことを確認します。
+  // toBe(null)は、返される値がnullであることをテストします。
   it('cannot inject `HighlightDirective` in 3rd <h2>', () => {
     const dir = bareH2.injector.get(HighlightDirective, null);
     expect(dir).toBe(null);
   });
 
-  // DebugElement.providerTokens
-  // attached HighlightDirective should be listed in the providerTokens
+  // テスト8: 1つ目の<h2>のproviderTokensにHighlightDirectiveが含まれていることを確認
+  // des[0].providerTokensは、最初のハイライトされた要素のプロバイダトークンを返します。これは、要素が依存するプロバイダのリストです。
+  // toContain(HighlightDirective)は、そのリストがHighlightDirectiveを含んでいるかどうかをテストします。
   it('should have `HighlightDirective` in 1st <h2> providerTokens', () => {
     expect(des[0].providerTokens).toContain(HighlightDirective);
   });
 
+  // テスト9: 3つ目の<h2>のproviderTokensにHighlightDirectiveが含まれていないことを確認
+  // not.toContain(HighlightDirective)は、そのリストがHighlightDirectiveを含んでいないかどうかをテストします。
   it('should not have `HighlightDirective` in 3rd <h2> providerTokens', () => {
     expect(bareH2.providerTokens).not.toContain(HighlightDirective);
   });
 });
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
